b72200f1d17c8e37b2f7244c4cf0c45cc92f2320
Anthony Fu <anthonyfu117@hotmail.com>
2021-07-23 14:02:31 +0800

    chore: fix links

--- views/components/Logo.vue
+++ views/components/Logo.vue
@@ -1,5 +1,5 @@
 <template>
-  <a class="icon-btn mx-2 text-2xl" rel="noreferrer" href="https://github.com/antfu/vitesse-lite" target="_blank" title="GitHub">
+  <a class="icon-btn mx-2 text-2xl" rel="noreferrer" href="https://github.com/antfu/vitesse-webext" target="_blank" title="GitHub">
     <pixelarticons-power />
   </a>
 </template>
