3cc3255aa218e4a2e847f2aee23a324c200b536c
Anthony Fu <anthonyfu117@hotmail.com>
2021-07-25 02:01:42 +0800

    fix: add message when vite does not start

--- scripts/prepare.ts
+++ scripts/prepare.ts
@@ -16,7 +16,9 @@ async function stubIndexHtml() {
   for (const view of views) {
     await fs.ensureDir(r(`extension/dist/${view}`))
     let data = await fs.readFile(r(`views/${view}/index.html`), 'utf-8')
-    data = data.replace('"./main.ts"', `"http://localhost:${port}/${view}/main.ts"`)
+    data = data
+      .replace('"./main.ts"', `"http://localhost:${port}/${view}/main.ts"`)
+      .replace('<div id="app"></div>', '<div id="app">Vite server did not start</div>')
     await fs.writeFile(r(`extension/dist/${view}/index.html`), data, 'utf-8')
     log('PRE', `stub ${view}`)
   }
