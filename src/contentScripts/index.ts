a3f4a7801443fac022c36a5c044139b350deff19
Anthony Fu <anthonyfu117@hotmail.com>
2021-08-24 22:57:23 +0800

    fix: use shadow dom to attach context vue, thanks to @tshemeng

--- src/contentScripts/index.ts
+++ src/contentScripts/index.ts
@@ -4,13 +4,17 @@ import { createApp } from 'vue'
 import 'vue-global-api'
 import App from './Content.vue'
 
-const app = document.createElement('div')
-document.body.appendChild(app)
-createApp(App).mount(app)
-
 console.info('[vitesse-webext] Hello world from content script')
 
 // communication example: send previous tab title from background page
 onMessage('tab-prev', ({ data }) => {
   console.log(`[vitesse-webext] Navigate from page "${data.title}"`)
 })
+
+// mount component to context window
+const container = document.createElement('div')!
+const shadowDOM = container.attachShadow?.({ mode: 'closed' }) || container
+const root = document.createElement('div')
+shadowDOM.appendChild(root)
+document.body.appendChild(container)
+createApp(App, { globalWindow: window }).mount(root)
