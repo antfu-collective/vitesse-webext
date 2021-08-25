e236c405f81d0dace7b97fefd9e2be5547631150
mengzhao <tshemeng@users.noreply.github.com>
2021-08-25 16:34:17 +0800

    chore: remove style.css inject code.

--- src/background/contentScriptHMR.ts
+++ src/background/contentScriptHMR.ts
@@ -9,8 +9,4 @@ browser.webNavigation.onCommitted.addListener(({ tabId, frameId }) => {
     file: 'dist/contentScripts/index.global.js',
     runAt: 'document_end',
   }).catch(error => console.error(error))
-  browser.tabs.insertCSS(tabId, {
-    file: 'dist/contentScripts/style.css',
-    runAt: 'document_start',
-  }).catch(error => console.error(error))
 })
