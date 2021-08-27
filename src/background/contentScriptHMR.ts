68e76c189cafe51822999888954d89f6f8fdbe4a
mengzhao <tshemeng@users.noreply.github.com>
2021-08-27 08:51:26 +0800

    fix: `chrome-extension://invalid/` issue

--- src/background/contentScriptHMR.ts
+++ src/background/contentScriptHMR.ts
@@ -1,15 +1,20 @@
+/* eslint-disable no-console */
 import browser from 'webextension-polyfill'
 import { detect } from 'detect-browser'
 
 const isFirefox = detect()?.name === 'firefox'
 
-browser.webNavigation.onCommitted.addListener(({ tabId, frameId }) => {
-  // Filter out non main window events.
-  if (frameId !== 0) return
+// Firefox fetchs files from cache instead of reloading changes from disk, hmr will not work as Chromium based browser
+if (!isFirefox) {
+  browser.webNavigation.onCommitted.addListener(({ tabId, frameId }) => {
+    // Filter out non main window events.
+    if (frameId !== 0) return
 
-  // inject the latest scripts
-  browser.tabs.executeScript(tabId, {
-    file: isFirefox ? '/' : './' + 'dist/contentScripts/index.global.js',
-    runAt: 'document_end',
-  }).catch(error => console.error(error))
-})
+    // inject the latest scripts
+    browser.tabs.executeScript(tabId, {
+      file: './dist/contentScripts/index.global.js',
+      runAt: 'document_end',
+    }).then(() => { console.log('contentScripts reloaded.') })
+      .catch(error => console.error(error))
+  })
+}
