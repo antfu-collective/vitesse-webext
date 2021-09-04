73be4a485f11a3f12a67ac27b3128c2072ad4faf
Mengzhao <tshemeng@users.noreply.github.com>
2021-09-04 09:11:40 +0800

    fix: contentscript inject ignores the browser's built-in urls

--- src/background/contentScriptHMR.ts
+++ src/background/contentScriptHMR.ts
@@ -2,9 +2,12 @@ import { isFirefox } from '~/env'
 
 // Firefox fetch files from cache instead of reloading changes from disk,
 // hmr will not work as Chromium based browser
-browser.webNavigation.onCommitted.addListener(({ tabId, frameId }) => {
+browser.webNavigation.onCommitted.addListener(({ tabId, frameId, url }) => {
   // Filter out non main window events.
   if (frameId !== 0)
+    return
+
+  if (url.startsWith('chrome://') || url.startsWith('chrome-extension://') || url.startsWith('devtools://'))
     return
 
   // inject the latest scripts
