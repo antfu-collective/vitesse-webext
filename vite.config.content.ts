6c844b998dfe196a93b5e1e76e196647626721cc
Tmk <i@tmk.im>
2021-10-04 12:07:59 +0800

    fix: change lib output mode (#40)

--- vite.config.content.ts
+++ vite.config.content.ts
@@ -3,6 +3,7 @@ import WindiCSS from 'vite-plugin-windicss'
 import { sharedConfig } from './vite.config'
 import { r, isDev } from './scripts/utils'
 import windiConfig from './windi.config'
+import packageJson from './package.json'
 
 // bundling the content script using Vite
 export default defineConfig({
@@ -22,11 +23,13 @@ export default defineConfig({
     sourcemap: isDev ? 'inline' : false,
     lib: {
       entry: r('src/contentScripts/index.ts'),
-      formats: ['es'],
+      name: packageJson.name,
+      formats: ['iife'],
     },
     rollupOptions: {
       output: {
         entryFileNames: 'index.global.js',
+        extend: true,
       },
     },
   },
