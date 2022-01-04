dda10b3a0671151f62731d53f52a2053e9e33624
hankolsen <henrik@hankolsen.com>
2022-01-04 14:52:00 +0100

    fix: use `storage.local` instead of `Window.localstorage` (#58)

--- src/logic/storage.ts
+++ src/logic/storage.ts
@@ -1,3 +1,3 @@
-import { useLocalStorage } from '@vueuse/core'
+import { useStorageLocal } from '~/composables/useStorageLocal'
 
-export const storageDemo = useLocalStorage('webext-demo', 'Storage Demo', { listenToStorageChanges: true })
+export const storageDemo = useStorageLocal('webext-demo', 'Storage Demo', { listenToStorageChanges: true })
