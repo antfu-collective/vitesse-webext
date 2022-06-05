c05e10e5c3bbe6db461768de3221c4c63c9e18e4
eugenesvk <eugenesvk@users.noreply.github.com>
2022-06-05 23:08:42 +0700

    remove illegal colons in path names on Windows

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -4,6 +4,7 @@ import { bgCyan, black } from 'kolorist'
 export const port = parseInt(process.env.PORT || '') || 3303
 export const r = (...args: string[]) => resolve(__dirname, '..', ...args)
 export const isDev = process.env.NODE_ENV !== 'production'
+export const isWin = process.platform === "win32";
 
 export function log(name: string, message: string) {
   // eslint-disable-next-line no-console
