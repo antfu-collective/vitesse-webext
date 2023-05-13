b6633673478215a638f48f12a9cdd0df7fa95967
birdgg <birdeggegg@gmail.com>
2023-05-13 23:22:47 +0800

    fix: support firefox background script

--- scripts/utils.ts
+++ scripts/utils.ts
@@ -4,6 +4,7 @@ import { bgCyan, black } from 'kolorist'
 export const port = parseInt(process.env.PORT || '') || 3303
 export const r = (...args: string[]) => resolve(__dirname, '..', ...args)
 export const isDev = process.env.NODE_ENV !== 'production'
+export const isFirefox = process.env.EXTENSION === 'firefox'
 
 export function log(name: string, message: string) {
   console.log(black(bgCyan(` ${name} `)), message)
